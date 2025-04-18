e3f51e4411d335c6680ac0dcfbf48108113b0722
MatinHosz <matin.hosseinzadeh06@gmail.com>
2025-04-18 11:43:12 +0330

    Create UserBehavior interface and create regular and premium behavior classes based on it

--- SpotifySim/src/core/PremiumBehavior.java
+++ SpotifySim/src/core/PremiumBehavior.java
@@ -1,4 +1,26 @@
 package core;
 
-public class PremiumBehavior {
+public class PremiumBehavior implements UserBehavior {
+    private int month;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+        // TODO complete method after completing User and Playlist class
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        // TODO complete method after completing Music class
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        int newMonthNumber = this.getMonth() + month;
+        this.setMonth(newMonthNumber);
+    }
+
+    public int getMonth() { return month; }
+    public void setMonth(int newMonthNumber) {
+        this.month = newMonthNumber;
+    }
 }
--- SpotifySim/src/core/RegularBehavior.java
+++ SpotifySim/src/core/RegularBehavior.java
@@ -1,4 +1,27 @@
 package core;
 
-public class RegularBehavior {
+public class RegularBehavior implements UserBehavior {
+    private int playingLimit = 5;
+
+    @Override
+    public void createPlaylist(String title, User owner) {
+        throw new InvalidOperatorException("Playlist creation is only available for premium users.");
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        if (this.getPlayingLimit() <= 0)
+            throw new InvalidOperatorException("You have reached your play limit.");
+        // TODO complete method after completing Music class
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        // TODO complete method after completing User class
+    }
+
+    public int getPlayingLimit() { return playingLimit; }
+    public void setPlayingLimit(int newLimitNumber) {
+        playingLimit = newLimitNumber;
+    }
 }
